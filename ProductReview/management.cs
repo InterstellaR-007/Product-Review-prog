81080157a17d1d13a289b686733b11a5ccbc9f35
unknown <intrstelr.007@gmail.com>
2020-11-06 01:19:58 +0530

    [Anuj] added only id and review

--- ProductReview/management.cs
+++ ProductReview/management.cs
@@ -9,10 +9,10 @@ namespace ProductReview
     {
         public void getQueriedList(List<ProductDetail> prodlist)
         {
-            var retreivedData = (from prodReviews in prodlist group prodReviews by prodReviews.ProductID into newProd orderby newProd.Key select newProd ).ToList();
+            var retreivedData = (from prodReviews in prodlist select prodReviews).ToList();
 
             foreach (var product in retreivedData)
-                Console.WriteLine("Product Id: {0}  Count: {1} ", product.Key,product.Count());
+                Console.WriteLine("Product Id: {0}  Review:{1} ", product.ProductID,product.Review);
         }
 
         public static void DisplayList(List<ProductDetail> productDetails)
